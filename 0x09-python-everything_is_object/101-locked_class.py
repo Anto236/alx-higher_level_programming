aff9a4c2e4e848e22c47293020e2caa0b0482f6d
Anto236 <Antonymbugua1237@gmail.com>
2023-01-03 16:10:40 +0000

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-""" this is a module that has a class that avoids
-dynamically creating attributes
-"""
+"""Locked class only containing attribute first_name"""
 
 
 class LockedClass:
     __slots__ = ["first_name"]
-    
-    pass
 
+    pass
