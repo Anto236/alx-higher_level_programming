6cde802fd1125d4ea50d8da460d5a6aafc326a9d
Anto236 <Antonymbugua1237@gmail.com>
2023-01-10 19:22:27 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Module with function that writes object to text file
+""" Module that writes an Object to a text file using
+a JSON representation
 """
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Writes an object to a text file, using a JSON representation
+    """ Function that writes an object to a text file
+    by a JSON representation
     Args:
-        my_obj: The object
-        filename: File to save my_obj to
+        my_obj: object
+        filename: textfile name
+    Raises:
+        Exception: when the object can't be encoded
     """
-
-    with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(my_obj))
+    with open(filename, 'w', encoding="utf-8") as f:
+        json.dump(my_obj, f)
