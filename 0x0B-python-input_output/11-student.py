e280d327c0a975a948565ddc34a1908969e140cc
Anto236 <Antonymbugua1237@gmail.com>
2023-01-10 19:33:14 +0000

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" Module that defines the class Student
+"""Module that holds class Student
 """
 
 
 class Student:
-    """ Class to create student instances """
+    """Defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """ Special method to initialize """
+        """Initializes Student"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """ Method that returns directory description """
-        return self.__dict__.copy()
+        """Retrieves a dictionary representation of a Student instance"""
+
+        return self.__dict__
