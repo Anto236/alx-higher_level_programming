3279c44e2c1c31b7667d77224daa8102695fbe11
Anto236 <Antonymbugua1237@gmail.com>
2023-01-10 19:34:15 +0000

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""Module that holds class Student
-"""
+
+"""Class Student definition"""
 
 
 class Student:
-    """Defines a student"""
+    """Student class"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes Student"""
+        """Initialization method"""
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Retrieves a dictionary representation of a Student instance"""
+    def to_json(self, attrs=None):
+        """Method to retrieve dictionary representation of Student instance"""
 
-        return self.__dict__
+        if attrs is None or type(attrs) is not list:
+            return (self.__dict__)
+        else:
+            x = {}
+            for y in self.__dict__:
+                for z in attrs:
+                    if y == z:
+                        x[y] = self.__dict__[y]
+        return (x)
+
+    def reload_from_json(self, json):
+        """Method to replace attributes"""
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
