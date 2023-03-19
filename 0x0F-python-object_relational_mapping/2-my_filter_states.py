47a67a575785187d246be60d19440ab9a85d8e96
Anto236 <Antonymbugua1237@gmail.com>
2023-03-19 10:44:11 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-""" Script that lists state based on argument passed"""
-
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    # Connect to database
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         port=3306)
+    # Create cursor
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states where name='{}'".format(argv[4]))
-    result = cursor.fetchall()
-    for row in result:
+
+    # Create SQL query using user input
+    sql = "SELECT * FROM states WHERE \
+           CONVERT(`name` USING Latin1) \
+           COLLATE Latin1_General_CS = '{}';".format(sys.argv[4])
+
+    # Execute SQL query
+    cursor.execute(sql)
+
+    # Fetch all the matching rows
+    rows = cursor.fetchall()
+
+    # Print rows
+    for row in rows:
         print(row)
 
+    # Close cursor and database connection
     cursor.close()
     db.close()
