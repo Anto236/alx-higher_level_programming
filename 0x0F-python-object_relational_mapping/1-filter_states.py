ec490a8f4eaf3ba29c130c003e63e358d5f2f76e
Anto236 <Antonymbugua1237@gmail.com>
2023-03-19 10:18:01 +0000

    task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all states with a name starting with N
-"""
-import sys
+""" Script that lists states starting with letter N from specified database"""
+
+
 import MySQLdb
+from sys import argv
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
-
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states where name REGEXP '^[N].*$' \
-                    ORDER BY id ASC""")
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
+                     ORDER BY id ASC""")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
 
     cursor.close()
     db.close()
