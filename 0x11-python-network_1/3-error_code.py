7aac6cba713e3716a60c01359b673fa504a4e43d
Anto236 <Antonymbugua1237@gmail.com>
2023-03-31 20:24:46 +0000

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,15 +5,15 @@ This module takes a URL, sends a request to the
 URL and displays the body of the response.
 """
 
-import sys
 import urllib.request
 import urllib.error
+import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
+    url = input("Enter a URL: ")
     try:
         with urllib.request.urlopen(url) as response:
-            print(response.read().decode('utf-8'))
+            body = response.read().decode('utf-8')
+            print(body)
     except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+        print(f"Error code: {e.code}")
