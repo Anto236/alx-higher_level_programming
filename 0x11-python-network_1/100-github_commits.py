50d17573fece45ca610a343c4c03912d69c1dcb6
Anto236 <Antonymbugua1237@gmail.com>
2023-03-31 23:12:58 +0000

    task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+a Python script that takes 2 arguments
+in order to solve this challenge.
+"""
+import requests
+import sys
+
+
+if len(sys.argv) < 3:
+    print("Usage: python3 :github_commits.py <repo_name> <owner_name>")
+    sys.exit(1)
+
+repo_name = sys.argv[1]
+owner_name = sys.argv[2]
+
+url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+
+response = requests.get(url)
+commits = response.json()
+
+for commit in commits[:10]:
+    sha = commit["sha"]
+    author_name = commit["commit"]["author"]["name"]
+    print(f"{sha}: {author_name}")
