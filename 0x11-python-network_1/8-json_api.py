5d4216dfcf0cc2072712de83ec7d05b9debeabf2
Anto236 <Antonymbugua1237@gmail.com>
2023-03-31 22:49:47 +0000

    task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This script sends a POST request to
+http://0.0.0.0:5000/search_user with
+a given letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
+    response = requests.post(url, data=data)
+
+    try:
+        result = response.json()
+    except ValueError:
+        print("Not a valid JSON")
+        sys.exit(1)
+
+    if result:
+        print("[{}] {}".format(result.get("id"), result.get("name")))
+    else:
+        print("No result")
